f461546050a1c7025ad20642bc6d61732f926054
CoderYounes <younesanas2021@gmail.com>
2024-01-29 23:37:48 +0100

    fix pycodestyle issue & add some documetation

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" Class Square with some Calculation Methods"""
+
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ initializaton for values"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +17,16 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Calculate the permeter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Print  with Humain readable format"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
+    """ Run as Main """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
